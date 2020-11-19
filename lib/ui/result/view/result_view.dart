2411633cc6ff8562f38673014704b93461cff402
Yusuf Demir <yusufdemir286634@gmail.com>
2020-11-19 06:29:42 +0200

    İnstagram Paylaşma Özelliği getirildi

--- lib/ui/result/view/result_view.dart
+++ lib/ui/result/view/result_view.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:instagram_share/instagram_share.dart';
 import 'package:intl/intl.dart';
+import 'package:screenshot/screenshot.dart';
 
 class ResultView extends StatefulWidget {
   final String name;
@@ -57,51 +61,77 @@ class _ResultViewState extends State<ResultView> {
     return false;
   }
 
+  ScreenshotController screenshotController = ScreenshotController();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text('Sonuç'),
-          backgroundColor: Colors.black87,
-        ),
-        body: Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                calculate()
-                    ? 'Dışarı Çıkabilirsiniz ${widget.name}'
-                    : 'Dışarı Çıkamazsınız ${widget.name}',
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 25.0,
-                    color: Colors.black87),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              SizedBox(
-                width: 200.0,
-                child: Container(
-                  height: 4,
-                  color: Colors.orange,
-                ),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Text(
-                  calculate()
-                      ? 'Lütfen Maskenizi Takınız Ve Sosyal Mesafeye Kuralına Uyunuz.'
-                      : 'Lütfen Evde Kalın.',
-                  style: TextStyle(fontSize: 25.0, color: Colors.black87),
-                ),
-              ),
-            ],
+    return Screenshot(
+      controller: screenshotController,
+      child: Scaffold(
+          appBar: AppBar(
+            title: Text('Sonuç'),
+            backgroundColor: Colors.black87,
           ),
-        ));
+          body: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  calculate()
+                      ? 'Dışarı Çıkabilirsiniz ${widget.name}'
+                      : 'Dışarı Çıkamazsınız ${widget.name}',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 25.0,
+                      color: Colors.black87),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                SizedBox(
+                  width: 200.0,
+                  child: Container(
+                    height: 4,
+                    color: Colors.orange,
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Text(
+                    calculate()
+                        ? 'Lütfen Maskenizi Takınız Ve Sosyal Mesafeye Kuralına Uyunuz.'
+                        : 'Lütfen Evde Kalın.',
+                    style: TextStyle(fontSize: 25.0, color: Colors.black87),
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 50),
+                  width: double.maxFinite,
+                  child: RaisedButton(
+                    onPressed: () async {
+                      await screenshotController.capture().then((File image) {
+                        InstagramShare.share(image.path, 'image');
+                      }).catchError((onError) {
+                        print(onError);
+                      });
+                    },
+                    padding: EdgeInsets.symmetric(vertical: 2),
+                    elevation: 0.0,
+                    color: Colors.black87,
+                    textColor: Colors.white,
+                    child: Text('İnstagramda Paylaş'),
+                  ),
+                )
+              ],
+            ),
+          )),
+    );
   }
 }
